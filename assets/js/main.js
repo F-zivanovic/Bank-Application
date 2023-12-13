e42c7b93b2ec93da3a3e9247f7d6df4ef1b10a67
F-zivanovic <filip.zivanovic1999@gmial.com>
2023-12-13 21:19:42 +0100

    Validate login form and login user

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 let usersDB = [];
 
+/*------------------------
+USERS REGISTRATION
+-------------------------*/
 let registerView = document.querySelector('.register__view');
-let loginView = document.querySelector('.login__view')
-
+let loginView = document.querySelector('.login__view');
+let adminView = document.querySelector('.admin__view');
 let registerBtn = document.querySelector('.register__btn');
 let registerForm = document.querySelector('.register__form');
 let registerViewBtn = document.querySelector('.register__viewBtn');
@@ -33,8 +36,44 @@ function validateRegisterForm(e){
 
 function saveUserInDB(a){
     usersDB.push(a);
-    registerForm.submit();
     registerView.style.display = 'none';
     loginView.style.display = 'block';
-    
+    // registerForm.submit();
+}
+
+
+/*------------------------
+USERS LOGIN
+-------------------------*/
+let loginBtn = document.querySelector('.login__btn');
+let loginForm = document.querySelector('.login__form');
+loginBtn.addEventListener('click', validateLoginForm);
+
+function validateLoginForm(e){
+    e.preventDefault();
+
+    let userName = loginForm.querySelector('input[name="username"]').value;
+    let password = loginForm.querySelector('input[name="password"]').value;
+
+    const foundUser = usersDB.find(user => user.userName === userName && user.password === password);
+
+    if((userName == '' && password == '') || (userName == '' || password == '') ){
+        alert('All fileds are required');
+    }
+    else if(foundUser){
+        loginView.style.display = 'none';
+        adminView.style.display = 'block';
+        createTable();
+    }
+    else{
+        showModalError();
+    }
+}
+
+function showModalError(){
+    let loginModal = document.querySelector(".login__modal");
+    let loginModalClose = document.querySelector(".login__modal__close");
+
+    loginModal.style.display = 'flex';
+    loginModalClose.addEventListener('click', () => loginModal.style.display = 'none');
 }
